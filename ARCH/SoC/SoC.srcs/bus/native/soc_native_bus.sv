977e48146638bebfacb150984af2f72198c2be6b
Yan Man <YANM0010@e.ntu.edu.sg>
2026-06-15 15:25:29 +0800

    Create empty soc_native_bus module
    Add empty soc_native_bus module for verification framework.

---
+++ ARCH/SoC/SoC.srcs/bus/native/soc_native_bus.sv
@@ -0,0 +1,5 @@
+module soc_native_bus(); endmodule
+
+// This file has no function but it is contained in the verification framework.
+// The verification program cannot work without this file.
+// So an empty module is left here.
