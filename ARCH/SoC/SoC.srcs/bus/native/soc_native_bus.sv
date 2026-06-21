da97d76d3e83358a5289d698f4f9b4cb9ff8026b
Yan Man <YANM0010@e.ntu.edu.sg>
2026-06-21 15:07:04 +0800

    Delete ARCH directory

--- ARCH/SoC/SoC.srcs/bus/native/soc_native_bus.sv
+++
@@ -1,5 +0,0 @@
-module soc_native_bus(); endmodule
-
-// This file has no function but it is contained in the verification framework.
-// The verification program cannot work without this file.
-// So an empty module is left here.
